d463316a318165b6fa117f778c16f91df0157654
bikilone <milosbirovljevic@gmail.com>
2019-09-27 18:55:50 +0200

    Basic style for 320/480px

--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,18 @@ module.exports = {
     rules: [
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      { test: /\.html$/, use: ["html-loader"] },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
       }
     ]
   }
