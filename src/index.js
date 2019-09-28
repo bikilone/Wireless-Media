6806197266b9f1d60c7b6a6d37ae2666c3dd7c43
bikilone <milosbirovljevic@gmail.com>
2019-09-28 20:03:20 +0200

    Added event listeners

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./styles/main.scss";
+
+const menu = document.getElementById("menu");
+const dropdown = document.getElementById("dropdown");
+const searchIcon = document.getElementById("search-icon");
+const input = document.getElementById("input");
+
+menu.addEventListener("click", event => {
+  dropdown.classList.toggle("nav-dropdown");
+  dropdown.classList.toggle("hide");
+});
+
+searchIcon.addEventListener("click", event => {
+  input.classList.toggle("search-input");
+  input.classList.toggle("hide");
+});
