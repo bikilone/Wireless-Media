d82faf87a7601cca2ccb7a7175a6962e18d30097
bikilone <milosbirovljevic@gmail.com>
2019-09-29 13:24:03 +0200

    Added css prefixes

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,17 @@ module.exports = {
 
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
+      },
       { test: /\.html$/, use: ["html-loader"] },
       {
         test: /\.(svg|png|jpg|gif|woff|ttf|eot|svg)$/,
