b9c089a644f2569c66e6e8724c3d80691745a78e
bikilone <milosbirovljevic@gmail.com>
2019-09-29 16:30:28 +0200

    Added html layout for slider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const menu = document.getElementById("menu");
 const dropdown = document.getElementById("dropdown");
 const searchIcon = document.getElementById("search-icon");
 const input = document.getElementById("input");
+const arrowLeft = document.getElementById("arrow-left");
+const arrowRight = document.getElementById("arrow-right");
+const carousel = document.getElementById("carousel");
+const slides = document.getElementsByClassName("text-slider");
 
 menu.addEventListener("click", event => {
   dropdown.classList.toggle("nav-dropdown");
@@ -13,4 +17,18 @@ menu.addEventListener("click", event => {
 searchIcon.addEventListener("click", event => {
   input.classList.toggle("search-input");
   input.classList.toggle("hide");
+});
+
+// Slide
+let counter = 0;
+const size = slides[0].clientWidth;
+
+arrowRight.addEventListener("click", () => {
+  arrowLeft.style.transform = `translateX(${size * counter} px)`;
+  // check if user can slide to right
+  if (counter < 2) {
+    carousel.style.transform = `translateX(${-size * counter} px)`;
+    console.log(carousel);
+    counter++;
+  }
 });
