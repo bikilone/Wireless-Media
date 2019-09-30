e1001929572f762036461be90e2da4dda8670fa3
bikilone <milosbirovljevic@gmail.com>
2019-09-30 08:49:24 +0200

    Added semi-functioning slider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const menu = document.getElementById("menu");
 const dropdown = document.getElementById("dropdown");
 const searchIcon = document.getElementById("search-icon");
 const input = document.getElementById("input");
+const sliders = document.getElementById("sliders");
+const sliderDot1 = document.getElementById("slider-dot-1");
+const sliderDot2 = document.getElementById("slider-dot-2");
+const sliderDot3 = document.getElementById("slider-dot-3");
+const arrowLeft = document.getElementById("arrow-left");
+const arrowRight = document.getElementById("arrow-right");
+const textSliders = document.getElementById("text-sliders");
 
 menu.addEventListener("click", event => {
   dropdown.classList.toggle("nav-dropdown");
@@ -13,4 +20,71 @@ menu.addEventListener("click", event => {
 searchIcon.addEventListener("click", event => {
   input.classList.toggle("search-input");
   input.classList.toggle("hide");
+});
+
+// Slider
+const numOfSlides = 3;
+const greenColor = "#bbd547";
+const grayColor = "#bababa";
+let counter = 0;
+
+window.addEventListener("resize", e => {
+  const sliderWidth = sliders.clientWidth / numOfSlides;
+  const textSliderWidth = textSliders.clientWidth / numOfSlides;
+  if (sliders.style.transform !== "") {
+    sliders.classList.remove("slider-transition");
+    sliders.style.transform = `translateX(${-sliderWidth}px)`;
+    setTimeout(() => {
+      sliders.classList.add("slider-transition");
+    }, 0);
+  }
+  if (textSliders.style.transform !== "") {
+    textSliders.classList.remove("slider-transition");
+    textSliders.style.transform = `translateX(${-textSliderWidth}px)`;
+    setTimeout(() => {
+      textSliders.classList.add("slider-transition");
+    }, 0);
+  }
+});
+
+arrowLeft.addEventListener("click", () => {
+  const size = sliders.clientWidth / numOfSlides;
+  if (counter === 0) {
+    return;
+  }
+  counter--;
+  textSliders.style.transform = `translateX(${-counter * size}px)`;
+  sliders.style.transform = `translateX(${-counter * size}px)`;
+});
+arrowRight.addEventListener("click", () => {
+  const size = sliders.clientWidth / numOfSlides;
+  if (counter === 2) {
+    return;
+  }
+  counter++;
+
+  textSliders.style.transform = `translateX(${-counter * size}px)`;
+  sliders.style.transform = `translateX(${-counter * size}px)`;
+});
+
+sliderDot1.addEventListener("click", () => {
+  const size = sliders.clientWidth / numOfSlides;
+  sliders.style.transform = "translateX(0)";
+  sliderDot1.style.borderColor = greenColor;
+  sliderDot2.style.borderColor = grayColor;
+  sliderDot3.style.borderColor = grayColor;
+});
+sliderDot2.addEventListener("click", () => {
+  const size = sliders.clientWidth / numOfSlides;
+  sliders.style.transform = `translateX(${-size}px)`;
+  sliderDot1.style.borderColor = grayColor;
+  sliderDot2.style.borderColor = greenColor;
+  sliderDot3.style.borderColor = grayColor;
+});
+sliderDot3.addEventListener("click", () => {
+  const size = sliders.clientWidth / numOfSlides;
+  sliders.style.transform = `translateX(${-size * 2}px)`;
+  sliderDot1.style.borderColor = grayColor;
+  sliderDot2.style.borderColor = grayColor;
+  sliderDot3.style.borderColor = greenColor;
 });
